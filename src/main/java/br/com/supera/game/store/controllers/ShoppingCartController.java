193ac5b272631f2ad14168dc92a2943a57f78fd5
Rafaela Dartora <siglyane@gmail.com>
2021-10-10 12:55:18 -0300

    Adiciona funcionalidades no shopping cart

--- src/main/java/br/com/supera/game/store/controllers/ShoppingCartController.java
+++ src/main/java/br/com/supera/game/store/controllers/ShoppingCartController.java
@@ -3,12 +3,15 @@ package br.com.supera.game.store.controllers;
 import br.com.supera.game.store.model.ItemsShoppingCart;
 import br.com.supera.game.store.model.Product;
 import br.com.supera.game.store.model.ShoppingCart;
+import br.com.supera.game.store.repository.ItemsShoppingCartRepository;
 import br.com.supera.game.store.repository.ProductRepository;
 import br.com.supera.game.store.repository.ShoppingCartRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.math.BigDecimal;
@@ -24,6 +27,11 @@ public class ShoppingCartController {
     @Autowired
     private ProductRepository productRepository;
 
+    @Autowired
+    private ItemsShoppingCartRepository itemsShoppingCartRepository;
+
+
+    @PostMapping
     public ResponseEntity<ShoppingCart> createNewShoppingCart() {
         try {
             //Funcionalidade: considerando que o ecommerce possui uma camada de usuario, verificar id existe no banco.
@@ -39,7 +47,7 @@ public class ShoppingCartController {
     }
 
     @PostMapping("/add-produto/{id}")
-    public ResponseEntity<Product> addItemShoppingCart(Long id, @RequestBody ItemsShoppingCart itemShoppingCart) {
+    public ResponseEntity addItemShoppingCart(Long id, @RequestBody ItemsShoppingCart itemShoppingCart) {
         try {
             Optional<ShoppingCart> shoppingCart = shoppingCartRepository.findById(id);
             Optional<Product> product = productRepository.findById(itemShoppingCart.getProduct().getId());
@@ -57,11 +65,12 @@ public class ShoppingCartController {
                 //Ao separar a camada de service e controller, o ideal seria fazer um método para
                 // a regra de negocio do frete.
                 shoppingCart.get().setShipment(shoppingCart.get().getShipment().add(BigDecimal.valueOf(10)));
-
-                return new ResponseEntity<>(HttpStatus.OK);
+                ;
+                return new ResponseEntity<>(itemsShoppingCartRepository.save(addNewItem), HttpStatus.CREATED);
 
             } else {
-
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Não é permitido adicionar novos itens" +
+                        "sem cumprir todos os requisitos.");
             }
 
         } catch (Exception e) {
@@ -69,7 +78,31 @@ public class ShoppingCartController {
         }
     }
 
-    public void freteRegra(){
+    @PutMapping("/finalizar/{id}")
+    public ResponseEntity<ShoppingCart> purchaseShoppingCart(@PathVariable("id") Long id) {
+        try {
+            Optional<ShoppingCart> shoppingCart = shoppingCartRepository.findById(id);
+            if(shoppingCart.isPresent() && !shoppingCart.get().isPurchased()) {
+                ShoppingCart shoppingCartUpdate = shoppingCart.get();
 
-  }
+                BigDecimal totalPrice = itemsShoppingCartRepository.valueTotalPrice(id);
+
+                shoppingCartUpdate.setSubtotalPrice(totalPrice);
+                int testFreeShipment = shoppingCartUpdate.getShipment().compareTo(BigDecimal.valueOf(250));
+                if(testFreeShipment >= 0){
+                    shoppingCartUpdate.setShipment(BigDecimal.valueOf(0));
+                }
+                shoppingCartUpdate.setTotalPrice(totalPrice.add(shoppingCartUpdate.getShipment()));
+                shoppingCartUpdate.setPurchased(true);
+
+                return new ResponseEntity<>(shoppingCartRepository.save(shoppingCartUpdate), HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
 }
--- src/main/java/br/com/supera/game/store/repository/ItemsShoppingCartRepository.java
+++ src/main/java/br/com/supera/game/store/repository/ItemsShoppingCartRepository.java
@@ -1,4 +1,14 @@
 package br.com.supera.game.store.repository;
 
-public interface ItemsShoppingCartRepository {
+import br.com.supera.game.store.model.ItemsShoppingCart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.math.BigDecimal;
+
+public interface ItemsShoppingCartRepository extends JpaRepository<ItemsShoppingCart, Long> {
+
+    @Query(value = "SELECT SUM(total_price) FROM items_shopping_cart where id_shopping_cart = :id_shopping_cart", nativeQuery = true)
+    public BigDecimal valueTotalPrice(@Param("id_shopping_cart") Long idShoppingCart);
 }
